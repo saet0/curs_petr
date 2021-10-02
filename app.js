ed7faa3a53c0d2abc94bf69e9a7929acf286c786
Aleksey <kaka_bubu@mail.ru>
2021-10-02 13:30:19 +1000

    lesson15 my decision

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-let a = prompt('Один из последних просмотренных фильмов?','');
-let b = prompt('На сколько оцените его?','')
+let numberOfFilms
+let a 
+let b 
+do {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    console.log(numberOfFilms)
+    a = prompt('Один из последних просмотренных фильмов?','');
+    console.log(a)
+    b = prompt('На сколько оцените его?') 
+    console.log(b)
+}
+while (numberOfFilms && a && b == false, a.length > 50);
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,3 +20,13 @@ let personalMovieDB = {
 };
 
 personalMovieDB.movies[a] = b;
+
+if (personalMovieDB.count <= 0) {
+    console.log('Произошла ошибка')
+} else if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов')
+} else if (personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else  {
+    console.log('Вы киноман')
+};
